4a8c987edfddbaa68d1ba7d38fc418afcaf602b0
Stroescu Tudor-Stefan <stroescutudor@yahoo.com>
2022-04-22 18:33:23 +0300

    Add role field in login response.
    Signed-off-by: Stroescu Tudor-Stefan <stroescutudor@yahoo.com>

--- src/main/java/com/bluesprint/orareacs/filter/FilterUtils.java
+++ src/main/java/com/bluesprint/orareacs/filter/FilterUtils.java
@@ -9,6 +9,7 @@ public class FilterUtils {
     public static final String ROLES = "roles";
     public static final String ACCESS_TOKEN = "accessToken";
     public static final String REFRESH_TOKEN = "refreshToken";
+    public static final String ROLE = "role";
     public static long ACCESS_TIME_MILLIS = 10 * 60 * 1000;
     public static long REFRESH_TIME_MILLIS = 7200 * 60 * 1000;
     public static List<String> PUBLIC_URLS = List.of("/api/register", "/api", "/api/login", "/api/refreshToken",
