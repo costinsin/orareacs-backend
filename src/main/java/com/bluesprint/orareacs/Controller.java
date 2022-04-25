2d7e4281ac5b5ed2b08f34fb2ead9cfbfd439fbf
Gagcex <a144185i@gmail.com>
2022-04-25 23:11:48 +0300

    test commit
    Signed-off-by: Gagcex <a144185i@gmail.com>

--- src/main/java/com/bluesprint/orareacs/Controller.java
+++ src/main/java/com/bluesprint/orareacs/Controller.java
@@ -22,4 +22,10 @@ public class Controller {
     public String testEveryoneAccess() {
         return "Everyone can access.";
     }
+
+    @PreAuthorize("hasAuthority('admin')")
+    @GetMapping("/api/test/add_orar")
+    public String testOrar() {
+        return "Orar.";
+    }
 }
