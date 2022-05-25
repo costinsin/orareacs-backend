8d485f241e0c1ebe27cd70dac7e7bed7fafa28a9
Gagcex <a144185i@gmail.com>
2022-05-25 13:33:33 +0300

    Get/Update timetable
    Signed-off-by: Andrei R. Stamate <a144185i@gmail.com>

--- src/main/java/com/bluesprint/orareacs/service/TimetableService.java
+++ src/main/java/com/bluesprint/orareacs/service/TimetableService.java
@@ -4,14 +4,19 @@ import com.bluesprint.orareacs.dto.Event;
 import com.bluesprint.orareacs.entity.Course;
 import com.bluesprint.orareacs.entity.Timetable;
 
+import java.sql.Time;
 import java.util.List;
+import java.util.Optional;
 
 public interface TimetableService {
     void addTimetable(Timetable timetable);
 
+    void updateTimetable(Timetable timetable);
     void deleteTimetableByGroup(String group);
 
     List<Event> getTimetable(String username);
+
+    Timetable getTimetableByGroup(String group);
 
     List<Course> getCourses(String username);
 
